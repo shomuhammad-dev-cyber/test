234fde2f6e0cf81e0611ec8106b68c1f15922381
Shomuhammad <xudayberdiyevshomuhammad@gmail.com>
2026-07-13 10:00:02 +0500

    page update

---
+++ src/app/about/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from './about.module.css'
+
+const About = () => {
+  return (
+    <div className={style.title}>About</div>
+  )
+}
+
+export default About
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>pagee</div>
+    <div>page</div>
   )
 }
 
